fd01522c24f9bc67a5ec2661ee5f50959f24d11e
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-04-04 12:49:06 +0500

    Update files

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header';
+import TodosLogic from './components/TodosLogic';
+import './components/style/style.css'
+
+const TodoApp = () => {
+  return (
+    <>
+      <Header />
+      <TodosLogic />
+    </>
+  );
+};
+export default TodoApp;
